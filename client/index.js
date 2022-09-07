c700981fdfac97a6c3ba5108bf8ef535dd4832d9
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-09-06 22:05:52 -0300

    Aula 4

--- client/index.js
+++ client/index.js
@@ -7,3 +7,21 @@ var campos = [
 console.log(campos)
 
 var tbody = document.querySelector('table tbody');
+
+document.querySelector('.form').addEventListener('submit', function (e) {
+    e.preventDefault;
+
+    var tr = document.createElement('tr');
+
+    campos.forEach(function (campos) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    var tdVolume = document.createElement('td')
+    tdVolume.textContent = campos[1].value * campos[2].value
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+})
