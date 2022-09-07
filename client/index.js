abc9f4189fb2e1181122448c0169826a3bf960b5
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2022-09-06 21:55:26 -0300

    Criando projeto

---
+++ client/index.js
@@ -0,0 +1,9 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+]
+;
+console.log(campos)
+
+var tbody = document.querySelector('table tbody');
